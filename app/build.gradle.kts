304862abe6cb623ed3ec75f890ecae0396f937ec
ksavaliya <karan.savaliya01@gmail.com>
2024-04-06 18:09:15 -0500

    Implemented MainPage.kt and routed to other activities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -51,11 +52,15 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -63,4 +68,13 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation ("com.google.android.gms:play-services-location:18.0.0")
+    implementation ("com.karumi:dexter:6.2.3")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("com.airbnb.android:lottie:5.0.3")
+    implementation ("com.google.android.material:material:<1.7.0-alpha01>")
+
+
 }
---
+++ app/src/main/java/com/example/fundsfly/buy_navi.kt
@@ -0,0 +1,11 @@
+package com.example.fundsfly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class buy_navi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_navi)
+    }
+}
